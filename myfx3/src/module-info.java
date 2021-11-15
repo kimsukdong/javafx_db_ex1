ccc68f3ae8ff420e6ad9c9a46a47ca0ea2c3f885
sukdong-kim <73944038+sukdong-kim@users.noreply.github.com>
2021-11-15 22:14:01 +0900

    db ex2

---
+++ myfx3/src/module-info.java
@@ -0,0 +1,8 @@
+module myfx3 {
+	requires javafx.controls;
+	requires javafx.fxml;
+	requires java.sql;
+	requires java.desktop;
+	
+	opens application to javafx.graphics, javafx.fxml, javafx.base;
+}
