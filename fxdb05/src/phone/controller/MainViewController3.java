ff0c20f054901b5eee37e4ac8fc8feb601a85c7a
sukdong-kim <73944038+sukdong-kim@users.noreply.github.com>
2021-11-15 22:06:34 +0900

    db ex

---
+++ fxdb05/src/phone/controller/MainViewController3.java
@@ -0,0 +1,52 @@
+package phone.controller;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.layout.AnchorPane;
+import phone.Main;
+
+public class MainViewController3 {
+	@FXML
+	AnchorPane root;
+    @FXML
+    void onClickAdd2(ActionEvent event) {
+		try {
+			root = FXMLLoader.load(getClass().getResource("/phone/view/Add.fxml"));
+			Main.mainLayout.setCenter(root);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+    @FXML
+    void onClickDelete2(ActionEvent event) {
+		try {
+			root = FXMLLoader.load(getClass().getResource("/phone/view/Delete.fxml"));
+			Main.mainLayout.setCenter(root);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+    @FXML
+    void onClickSearch2(ActionEvent event) {
+		try {
+			root = FXMLLoader.load(getClass().getResource("/phone/view/Search.fxml"));
+			Main.mainLayout.setCenter(root);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+    @FXML
+    void onClickUpdate2(ActionEvent event) {
+		try {
+			root = FXMLLoader.load(getClass().getResource("/phone/view/Update.fxml"));
+			Main.mainLayout.setCenter(root);
+		} catch(Exception e) {
+			e.printStackTrace();
+		}
+    }
+
+}
